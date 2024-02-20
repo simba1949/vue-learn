1113a6f58aa2bf3364eab594e0487131fec5ce2f
openpark <openpark.vip@outlook.com>
2024-02-20 23:02:30 +0800

    feature：添加登录页的背景图片 feature：删除 vue-cli 自带的样式

--- src/views/10ElementPlusView/LoginView.vue
+++ src/views/10ElementPlusView/LoginView.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="common-layout" >
-      <el-container class="login-main">
-        <el-main>Main</el-main>
-      </el-container>
-    </div>
-  </template>
+  <div class="common-layout">
+    <el-container class="login-main">
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
 
 <style>
 .login-main {
-    background-image: url('@/assets/loginBackground.png');
-    background-size: 100% 100%;
+  background-image: url('@/assets/loginBackground.png');
+  height: 100vh;
+  background-size: 100% 100%;
 }
 </style>
