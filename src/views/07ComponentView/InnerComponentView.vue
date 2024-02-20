7c9faa9550fba97d3fa53a6546304cfd7c5fc7c7
anthony <anthony1949@zto.com>
2024-02-20 15:43:30 +0800

    子组件向父组件传播事件

--- src/views/07ComponentView/InnerComponentView.vue
+++ src/views/07ComponentView/InnerComponentView.vue
@@ -9,6 +9,12 @@
     <div>
         {{ msg }}
     </div>
+
+    <button @click="() => { msg = '君不见黄河之水天上来' }">change</button>
+
+    <div>
+        父组件调用子组件方法的传参：{{ fromParentVal }}
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -23,4 +29,17 @@ defineProps({
         default: 18
     }
 })
+
+// 定义变量
+let fromParentVal: any;
+// 定义方法
+function printFun(val: any) {
+    console.log('我被父组件调用了');
+    console.log(val)
+    fromParentVal = val;
+}
+// 暴露方法给父组件调用
+defineExpose({
+    printFun
+});
 </script>
--- src/views/07ComponentView/OuterComponentView.vue
+++ src/views/07ComponentView/OuterComponentView.vue
@@ -1,8 +1,9 @@
 <template>
     <!-- 使用内层组件视图 -->
     <!-- 引入子组件，bing属性绑定到子组件的自定义属性上（例如:age） -->
-    <InnerComponentView :age="count" />
-    <button @click="incrementValue">点击</button>
+    <InnerComponentView ref="sonComponent" :age="count" /><br>
+    <button @click="incrementValue">点击</button><br>
+    <button @click="callSon">调用子组件方法</button><br>
 </template>
 
 <script setup lang="ts">
@@ -18,5 +19,14 @@ const count = ref(0);
 function incrementValue() {
     console.log('count', count.value);
     count.value++;
+}
+
+// 调用子组件核心方法
+// 引用子组件对象，将类型设置为允许为 null
+const sonComponent = ref<null | any>(null)
+// 调用子组件方法
+function callSon() {
+    console.log('我去调用子组件了');
+    sonComponent.value.printFun(count.value);
 }
 </script>
