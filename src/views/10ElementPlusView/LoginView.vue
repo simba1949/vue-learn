289999472ee9cbcadd58e834b8946f515f602dab
openpark <openpark.vip@outlook.com>
2024-02-21 12:22:09 +0800

    feature：登录表单居中，添加登录方法的框架

--- src/views/10ElementPlusView/LoginView.vue
+++ src/views/10ElementPlusView/LoginView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="login-container">
     <el-form
+        id="loginForm"
         :label-position="'right'"
         label-width="100px"
         :model="formLabelAlign"
-        style="max-width: 460px;"
-
     >
       <el-form-item label="用户名">
-        <el-input v-model="formLabelAlign.username" type="text"/>
+        <el-input v-model="formLabelAlign.username" type="text" placeholder="请输入用户名" clearable/>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formLabelAlign.password" type="password"/>
+        <el-input v-model="formLabelAlign.password" type="password" placeholder="请输入密码" clearable show-password/>
       </el-form-item>
+      <el-form-item>
+        <el-row class="mb-24" style="margin-left: 20%;">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info">忘记密码</el-button>
+        </el-row>
+      </el-form-item>
+
     </el-form>
   </div>
 
@@ -25,6 +31,10 @@ const formLabelAlign = reactive({
   username: '',
   password: ''
 })
+
+function login() {
+  console.log("去登录啦")
+}
 </script>
 
 
@@ -33,5 +43,16 @@ const formLabelAlign = reactive({
   background-image: url('@/assets/loginBackground.png');
   height: 100vh;
   background-size: 100% 100%;
+}
+
+/* 登录表单在页面居中显示 */
+#loginForm {
+  max-width: 460px;
+  /* 决定元素的定位方式：绝对定位 absolute、相对定位 relative、固定定位 fixed、静态定位 static */
+  position: relative;
+  /* 定位=定位模式 + 边偏移（top、bottom、left、right）*/
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
