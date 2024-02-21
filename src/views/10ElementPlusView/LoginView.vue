7a3e2c4a832a37e7f2e32415c9936c930d317eca
anthony <anthony1949@zto.com>
2024-02-21 11:21:19 +0800

    feature：提交代码

--- src/views/10ElementPlusView/LoginView.vue
+++ src/views/10ElementPlusView/LoginView.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="common-layout">
-    <el-container class="login-main">
-      <el-main>Main</el-main>
-    </el-container>
+  <div class="login-container">
+    <el-form
+        :label-position="'right'"
+        label-width="100px"
+        :model="formLabelAlign"
+        style="max-width: 460px;"
+
+    >
+      <el-form-item label="用户名">
+        <el-input v-model="formLabelAlign.username" type="text"/>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.password" type="password"/>
+      </el-form-item>
+    </el-form>
   </div>
+
 </template>
 
+<script lang="ts" setup>
+import {reactive, ref} from 'vue'
+
+const formLabelAlign = reactive({
+  username: '',
+  password: ''
+})
+</script>
+
+
 <style>
-.login-main {
+.login-container {
   background-image: url('@/assets/loginBackground.png');
   height: 100vh;
   background-size: 100% 100%;
